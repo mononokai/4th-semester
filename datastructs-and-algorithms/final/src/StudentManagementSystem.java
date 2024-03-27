101bde43c04826a17a38f3203e75a3dfbaa6051d
david <leachda12@icloud.com>
2024-03-27 14:32:16 -0400

    changed file name and added comments

--- datastructs-and-algorithms/final/src/StudentManagementSystem.java
+++ datastructs-and-algorithms/final/src/StudentManagementSystem.java
@@ -1,16 +1,19 @@
 import java.util.Arrays;
 
-public class School {
+public class StudentManagementSystem {
+    // array for storing students
     Student[] students;
 
     public void add(int ID, String name, int age, int grade) {
         Student student = new Student(ID, name, age, grade);
-
+        
+        // copy and extend students array to add new student
         students = Arrays.copyOf(students, students.length + 1);
         students[students.length - 1] = student;
     }
 
     public int searchByID(int ID) {
+        // linear search for student ID
         for (int i = 0; i < students.length; i++) {
             if (students[i].ID == ID)
                 return i;
@@ -20,6 +23,7 @@ public class School {
     }
 
     public int searchByName(String name) {
+        // linear search for student name
         for (int i = 0; i < students.length; i++) {
             if (students[i].name == name)
                 return i;
@@ -29,20 +33,22 @@ public class School {
     }
 
     public void delete(int ID) {
+        // use search method to find student index
         int index = searchByID(ID);
 
+        // shift values down one index
         for (int i = index; i < students.length - 1; i++) {
             students[i] = students[i + 1];
         }
 
+        // remove last element in the array
         students = Arrays.copyOf(students, students.length - 1);
     }
 
     public void display() {
+        // iterate through students, printing their ID and name
         for (Student student : students) {
             System.out.println("Student ID: " + student.ID + ", name: " + student.name);
         }
     }
-
-    
 }
